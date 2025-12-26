7b86c5c685362b60b31ab47c5c32560d2326dd6c
Jason Majoros <jemajoros@gmail.com>
2025-12-26 00:56:46 -0500

    compiler

--- src/util/error.hpp
+++ src/util/error.hpp
@@ -7,19 +7,17 @@
 
 namespace util {
 
-__attribute__((noinline))
-[[noreturn]] inline void die(std::string_view msg) {
+[[noreturn]] static inline void die(std::string_view msg) {
   std::fprintf(stderr, "%s\n", msg.data());
   std::exit(EXIT_FAILURE);
 }
 
-__attribute__((noinline))
-[[noreturn]] inline void die_errno(std::string_view msg) {
+[[noreturn]] static inline void die_errno(std::string_view msg) {
   std::perror(std::string(msg).c_str());
   std::exit(EXIT_FAILURE);
 }
 
-inline void syscall_or_die(int result, std::string_view msg) {
+static inline void syscall_or_die(int result, std::string_view msg) {
   if (result == -1) {
     die_errno(msg);
   }
